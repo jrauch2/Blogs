669e17baeaf0b4b099fb0b4702f3206c969b160d
jgrissom <jgrissom91@gmail.com>
2019-02-27 18:53:01 -0600

    Display all Blogs
    1) Update the Startup class
    2) Update the Home Controller
    3) Update the Index View

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blogs.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blogs.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index() => View();
+        // this controller depends on the BloggingRepository
+        private IBloggingRepository repository;
+        public HomeController(IBloggingRepository repo) => repository = repo;
+
+        public IActionResult Index() => View(repository.Blogs);
     }
 }
