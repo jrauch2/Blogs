8df3e668e16550a1276537d9899c78cd9194374c
jgrissom <jgrissom91@gmail.com>
2019-03-23 08:41:14 -0500

    Admin Controller
    1) Create User Manager which will be used to query the Identity database
    2) Create Index controller method to return the collection of users from the DB to the view

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Blogs.Models;
+
+namespace Blogs.Controllers
+{
+    public class AdminController : Controller
+    {
+        private UserManager<AppUser> userManager;
+
+        public AdminController(UserManager<AppUser> usrMgr)
+        {
+            userManager = usrMgr;
+        }
+
+        public ViewResult Index() => View(userManager.Users);
+    }
+}
