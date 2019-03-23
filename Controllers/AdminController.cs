0dcdc1b9f5d134f748fbecebb6c39bf5b2f73ba4
jgrissom <jgrissom91@gmail.com>
2019-03-23 09:19:59 -0500

    Admin/Delete
    Add a Controller method to the Admin controller to handle post requests for Admin/Delete

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -44,5 +44,36 @@ namespace Blogs.Controllers
             }
             return View(model);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            AppUser user = await userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                IdentityResult result = await userManager.DeleteAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "User Not Found");
+            }
+            return View("Index", userManager.Users);
+        }
+
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+        }
     }
 }
